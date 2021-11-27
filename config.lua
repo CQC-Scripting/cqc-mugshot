a806f4bd9d0ca27e85591844e38eb1eabde61ecc
Quinn101 <michaelq208@gmail.com>
2021-11-27 10:19:44 -0800

    CQC Config

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
-Config.Webhook = 'https://discord.com/api/webhooks/913767420794122240/3f_yIYDco5z4m_8IabNZ27b2z3X8zvW7nGgCHWvjjU_8_zlzSrfh-kDdA3daluJcXxn5' -- Images will be uploaded here
-Config.TestCommand -- Use this when testing /testmugshot
+Config.Webhook = '' -- Images will be uploaded here
+Config.TestCommand = false -- Use this when testing /testmugshot
 
 Config.MugshotLocation = vector3(-559.81, -132.68, 32.75) -- Location of the Suspect
 Config.MugshotHeading = 291.18 -- Direction Suspsect is facing
@@ -15,4 +15,4 @@ Config.MugShotCamera = {
 Config.BoardHeader = "Los Santos Police Department" -- Header that appears on the board
 Config.WaitTime = 2000 -- Time before and after the photo is taken. Decreasing this value might result in some angles being skiped.
 Config.Photos = 3 -- Front, Back Side. Use 4 for both sides
-Config.CQCMDT = true -- If you use CQC MDT this will automatically send them to a players profile
+Config.CQCMDT = false -- If you use CQC MDT this will automatically send them to a players profile
