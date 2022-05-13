cb1596e70df5b6f420574c2780e331f6441bd2e1
Fast-Lane RolePlay <timothyphilipp14@gmail.com>
2022-05-12 23:56:23 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -13,6 +13,21 @@ Config.MugShotCamera = {
     r = {x = 0.0, y = 0.0, z = 110.33} -- To change the rotation use the z. Others are if you want rotation on other axis
 }
 
+--[[
+
+If using Western's MRPD the Coordinates are as following,
+
+Config.MugshotLocation = vector3(484.0772, -999.4976, 25.4675) -- Location of the Suspect
+Config.MugshotSuspectHeading = 357.8132 -- Direction Suspsect is facing
+Config.MugShotCamera = {
+    x = 484.0522,
+    y = -997.6379,
+    z = 25.4675,
+    r = {x = 0.0, y = 0.0, z = 177.5} -- To change the rotation use the z. Others are if you want rotation on other axis
+}
+
+]]
+
 Config.BoardHeader = "Los Santos Police Department" -- Header that appears on the board
 Config.WaitTime = 2000 -- Time before and after the photo is taken. Decreasing this value might result in some angles being skiped.
 Config.Photos = 3 -- Front, Back Side. Use 4 for both sides
